c959698692f2747bac59e707e284f8c6e9772fad
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-10-03 14:00:34 -0600

    Created base project

---
+++ src/components/ReactComponentTest/ReactComponentTest.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './test.css';
+
+const ReactComponentTest = () => {
+  return (
+    <div className='test'>Hi there! This is a test of creating react components!!!</div>
+  )
+}
+
+export default ReactComponentTest
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactComponentTest from './components/ReactComponentTest/ReactComponentTest';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ReactComponentTest/>
   </React.StrictMode>
 );
 
