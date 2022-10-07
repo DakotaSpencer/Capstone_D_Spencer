f89afc8f2a702a034296806e4d272f04d259b4d0
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-10-07 12:49:34 -0600

    Fixed some stuff with color generation and formatting

--- frontend/src/components/Color.js
+++ frontend/src/components/Color.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './ColorList.css';
+import {useEffect, useState} from 'react'
+
+
 
 const Color = ({color}) => {
+  const [hexShown, setHexShown] = useState(true);
   return (
-    <div style={{backgroundColor : `${color.hex.value}`, height:400} }>
+    <div className= {color.hsl.l > 50 ? "text-dark text-weight-thick" : "text-light text-weight-thick"} style={{backgroundColor : `${color.hex.value}`, height:400} }>
         <div className='text-center text-size-medium'>
-            {color.hex.value}
+            <div>{color.name.value}</div>
+            {hexShown? color.hex.value: color.hex.clean}
         </div>
     </div>
   )
--- frontend/src/components/PaletteGenerator.js
+++ frontend/src/components/PaletteGenerator.js
@@ -5,9 +5,9 @@ import {useEffect, useState} from 'react'
 import ColorList from './ColorList';
 
 const PaletteGenerator = () => {
-    const [hexCode, setHexCode] = useState(Math.floor(Math.random()*16777215).toString(16));
+    const [hexCode, setHexCode] = useState(Math.floor(Math.random()*16777215).toString(16).toUpperCase());
     const [generationMode, setGenerationMode] = useState('analogic');
-    const [colorCount, setColorCount] = useState('1');
+    const [colorCount, setColorCount] = useState('');
     const [colordata, setColorData] = useState([]);
   
     useEffect(() => {
@@ -15,15 +15,17 @@ const PaletteGenerator = () => {
     },[])
   
     const getData = async () => {
-      console.log(hexCode)
-      //https://www.thecolorapi.com/scheme?hex=${this.hexcolor}&mode=${this.selectedMode}&count=${this.numOfColors}
-      const result = await axios.get(`https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${generationMode}&count=${colorCount}`)
-      console.log(result.data.colors)
-      setColorData(result.data.colors)
+        console.log(hexCode)
+        //https://www.thecolorapi.com/scheme?hex=${this.hexcolor}&mode=${this.selectedMode}&count=${this.numOfColors}
+        const result = await axios.get(`https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${generationMode}&count=${colorCount}`)
+        console.log(`https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${generationMode}&count=${colorCount}`)
+        console.log(result.data.colors)
+        setColorData(result.data.colors)
     }
   
     const handleSearch = (e) => {
       e.preventDefault();
+      
       //Default is to do a postback, which is refreshing the page, which we dont want
       //preventDefault() says dont post back, and instead do a console log
       console.log('User Submitted My Form!');
@@ -45,17 +47,17 @@ const PaletteGenerator = () => {
             <label>Count</label>
   
             <input type='number' value={colorCount} placeholder='5'
-              onChange={e => setColorCount(e.target.value)}/>
+              onChange={e => e.target.value > 10 ? setColorCount(10) : setColorCount(e.target.value) | e.target.value < 1 ? setColorCount(1) : setColorCount(e.target.value)}/>
   
   
             <label>Generation Mode</label>
             <select id="modeSelect" value={generationMode} onChange={e => setGenerationMode(e.target.value)}>
+              <option value="analogic">analogic</option>
+              <option value="analogic-complement">analogic-complement</option>
+              <option value="complement">complement</option>
               <option value="monochrome">monochrome</option>
               <option value="monochrome-dark">monochrome-dark</option>
               <option value="monochrome-light">monochrome-light</option>
-              <option value="analogic">analogic</option>
-              <option value="complement">complement</option>
-              <option value="analogic-complement">analogic-complement</option>
               <option value="triad">triad</option>
               <option value="quad">quad</option>
             </select>
