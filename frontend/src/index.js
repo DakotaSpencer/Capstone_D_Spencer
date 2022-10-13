d3effd4fcf33191360681657e14dc2f8e8ea7ee4
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-10-13 09:57:22 -0600

    Updated styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,10 +10,8 @@ import { ImagePalette } from './components/ImagePalette/ImagePalette';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    
-    <ImagePalette/>
     <PaletteGenerator/>
+    <ImagePalette/>
   </React.StrictMode>
 );
 
