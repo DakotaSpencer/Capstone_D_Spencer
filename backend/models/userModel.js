17846cd610d5dc2949d8a303037b6e3504576655
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-11-03 20:43:43 -0600

    started implementation of user signup on backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    // username:{
-    //     type: String,
-    //     required: false,
-    //     //change required to True later and update accordingly
-    //     unique: false
-    // },
+    username:{
+        type: String,
+        required: false,
+        //change required to True later and update accordingly
+        unique: false
+    },
     email:{
         type: String,
         required: true,
@@ -19,5 +20,26 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// Static signup method
+userSchema.statics.signup = async function(email, password) {
+    const exists = await this.findOne({email})
+
+    if (exists){
+        throw Error('Email is already in use.')
+    }
+
+    //salt and hash password, and store in database
+
+    //generates salt
+    const salt = await bcrypt.genSalt(10)
+    //hashes password
+    const hash = await bcrypt.hash(password, salt)
+
+    //store in database alongside email
+    const user = await this.create({email, password: hash})
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
